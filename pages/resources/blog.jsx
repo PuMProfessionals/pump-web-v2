53686e0cd05a34c8db0db272deeb1fce8671ef74
xsharonhe <xsharonhe@gmail.com>
2021-07-23 02:10:57 -0400

    Rename

---
+++ pages/resources/blog.jsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+
+export default function Blog({
+    ...props
+}) {
+    return (
+        <Wrapper {...props}>
+            hello
+        </Wrapper>
+    );
+}
+
+const Wrapper = styled.div`
+`;
