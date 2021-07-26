28c21840412043fc1e067329ac7fc091cfbaa651
xsharonhe <xsharonhe@gmail.com>
2021-07-26 14:44:00 -0400

    Fixing margins

--- sections/home/WhatsHappening.jsx
+++ sections/home/WhatsHappening.jsx
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 import PumpDigest from "../../public/home/pump-digest.png";
 import ImpactReport from "../../public/home/impact-report.png";
-import { CardCarousel, Text, Video} from "../../components";
+import { CardCarousel, Text } from "../../components";
 import { baseTheme } from "../../theme";
 import { media } from "../../utils";
 
@@ -12,8 +12,6 @@ export const WhatsHappening = ({ ...props }) => (
     <Title size={baseTheme.size.h1} bold="true">
       What&apos;s Happening?
     </Title>
-
-
     <CarouselWrapper>
       <CardCarousel
         slides={[
@@ -50,12 +48,11 @@ export const WhatsHappening = ({ ...props }) => (
         ]}
       />
     </CarouselWrapper> 
-    <Video videoLink = "https://www.youtube.com/watch?v=f5jsJWl5WS0" titleText = "Get Started With UAS" descriptionText = "Watch this quick guide on how to navigate the UAS Discord Server to get started!"  />
   </Wrapper>
 );
 
 const Wrapper = styled.div`
-  margin-bottom: 15vh;
+  margin-bottom: 10vh;
   padding: 0 2%;
   ${media(
     1400,
