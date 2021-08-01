4e0c27bbae065ee3a0732191a172902d94f782b3
Sharon He <xsharonhe@gmail.com>
2021-08-01 01:16:42 -0400

    Fix home page (#21)
    * Main home page section feedback changes
    
    * Release pumprofessionals.org changes
    
    * Prettier fixes
    
    * Restore .env

--- utils/strings.js
+++ utils/strings.js
@@ -1,12 +1,12 @@
 /*
- * Constants / any strings in pump-ui-library
+ * Constants / any strings
  */
 export const CONSTANTS = {
   pump: "Prospective Medical Professionals",
   email: "info@pumprofessionals.org",
   facebook: "https://www.facebook.com/pumprofessionals/",
   instagram: "https://www.instagram.com/pumpprofessionals/?hl=en",
-  resources: ["Home", "About Us", "Resources", "Events", "Partners", "Donate"],
+  resources: ["Home", "About Us", "Resources", "Events", "Sponsors"],
   registered_charity_descr:
     "A registered charity dedicated to connecting aspiring students to health sciences opportunities.",
   copyright: "© 2021 Prospective Medical Professionals",
