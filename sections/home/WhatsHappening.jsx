4db5651feb8966066cd109da41ae19c240d0aa9f
Amatol <ravenlesswonders@gmail.com>
2021-08-02 14:11:47 -0400

    added a loading component

--- sections/home/WhatsHappening.jsx
+++ sections/home/WhatsHappening.jsx
@@ -2,12 +2,13 @@ import styled from "styled-components";
 
 import PumpDigest from "../../public/home/pump-digest.png";
 import ImpactReport from "../../public/home/impact-report.png";
-import { CardCarousel, Text } from "../../components";
+import { CardCarousel, Text, Loading } from "../../components";
 import { baseTheme } from "../../theme";
 import { media } from "../../utils";
 
 export const WhatsHappening = ({ ...props }) => (
   <Wrapper {...props}>
+    <Loading color = {baseTheme.colors.navy}></Loading>
     <Title size={baseTheme.size.h1} bold="true">
       What&apos;s Happening?
     </Title>
@@ -47,6 +48,7 @@ export const WhatsHappening = ({ ...props }) => (
         ]}
       />
     </CarouselWrapper>
+    
   </Wrapper>
 );
 
