f2afc3b8a4631de53bf335623c3c77b5fb892cb2
hirundinidae <diana4lin@gmail.com>
2021-08-07 16:04:03 -0400

    Import loading component on home page and lint

--- components/Loading.jsx
+++ components/Loading.jsx
@@ -2,21 +2,16 @@ import styled from "styled-components";
 import Loader from "react-loader-spinner";
 
 export const Loading = ({
-    type = "ThreeDots",
-    color,
-    height = 80,
-    width = 80,
-    ...props
+  type = "ThreeDots",
+  color,
+  height = 80,
+  width = 80,
+  ...props
 }) => (
-    <Wrapper {...props}>
-        <Loader
-            type={type}
-            color={color}
-            height={height}
-            width={width}
-        />
-    </Wrapper>
-)
+  <Wrapper {...props}>
+    <Loader type={type} color={color} height={height} width={width} />
+  </Wrapper>
+);
 const Wrapper = styled.div`
-    padding: 4vh 4vw;
+  padding: 4vh 4vw;
 `;
