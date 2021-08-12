4511ec49f0882c8b581e3c1d1ed89f29710a4b8c
hirundinidae <diana4lin@gmail.com>
2021-08-12 00:00:57 -0400

    Add proptypes and comments

--- components/Author.jsx
+++ components/Author.jsx
@@ -1,8 +1,18 @@
 import Image from "next/image";
 import styled from "styled-components";
+import PropTypes from "prop-types";
 
 import { Text } from "../components";
 import { media } from "../utils";
+
+/**
+ * Blog author component
+ *
+ * @prop {object} avatar - Profile picture image
+ * @prop {array} names - list of author names
+ *
+ * see example on blog.jsx (line 70)
+ */
 
 export const Author = ({ avatar, names }) => (
   <Wrapper>
@@ -39,3 +49,8 @@ const Avatar = styled.div`
 const Names = styled.div`
   word-break: break-work;
 `;
+
+Author.propTypes = {
+  avatar: PropTypes.object,
+  names: PropTypes.array,
+};
