65fb5dec996d72118e5789feac107bb60e758a99
xsharonhe <xsharonhe@gmail.com>
2021-08-12 21:52:02 -0400

    Prettier fixes

--- pages/resources/blog/[slug].jsx
+++ pages/resources/blog/[slug].jsx
@@ -48,7 +48,7 @@ export const getStaticPaths = async () => {
     postPaths = posts;
   }
 
-  postPaths = postPaths.filter(postPath => postPath.published);
+  postPaths = postPaths.filter((postPath) => postPath.published);
 
   const paths = postPaths.map((post) => ({
     params: {
