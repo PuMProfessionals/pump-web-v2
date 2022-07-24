443b625381ae5eb6f1510e584a8d768d5ffe0fb1
Sharon He <xsharonhe@gmail.com>
2022-07-23 23:37:55 -0400

    fix: lint

--- pages/scholarship.jsx
+++ pages/scholarship.jsx
@@ -13,20 +13,20 @@ export default function Resources({ ...props }) {
       </Head>
       <PageLayout>
         <iframe
-            id="JotFormIFrame-221947443957064"
-            title="PuMP Foundation Scholarship"
-            onLoad="window.parent.scrollTo(0,0)"
-            allowTransparency="true"
-            allowFullScreen="true"
-            allow="geolocation; microphone; camera"
-            src="https://form.jotform.com/221947443957064"
-            frameBorder="0"
-            style={{
-                minWidth: "100%",
-                height: "539px",
-                border: "none",
-                scrolling: "no"
-            }}
+          id="JotFormIFrame-221947443957064"
+          title="PuMP Foundation Scholarship"
+          onLoad="window.parent.scrollTo(0,0)"
+          allowTransparency="true"
+          allowFullScreen="true"
+          allow="geolocation; microphone; camera"
+          src="https://form.jotform.com/221947443957064"
+          frameBorder="0"
+          style={{
+            minWidth: "100%",
+            height: "539px",
+            border: "none",
+            scrolling: "no",
+          }}
         />
       </PageLayout>
     </div>
