06710667a974b8fce2f8b1cf9923bed7a0bc008b
gosqib <69024184+gosqib@users.noreply.github.com>
2023-06-10 16:07:27 -0400

    update markdown editor placeholder

--- components/MDEditor.js
+++ components/MDEditor.js
@@ -4,32 +4,36 @@ import "easymde/dist/easymde.min.css";
 export default function MDEditor({ initialValue, mdeRecordSetter }) {
   const [mde, setMde] = useState(null);
 
-  const injectEditor = useCallback(async (element) => {
-    if (element == null) return;
+  const injectEditor = useCallback(
+    async (element) => {
+      if (element == null) return;
 
-    // create and add editor
-    element.innerHTML = "";
-    const editor = document.createElement("textarea");
-    element.append(editor);
+      // create and add editor
+      element.innerHTML = "";
+      const editor = document.createElement("textarea");
+      element.append(editor);
 
-    // dynamic import to to support SSR
-    const EasyMDE = (await import("easymde")).default;
-    const easyMDE = new EasyMDE({
-      element: editor,
-      showIcons: [
-        "strikethrough",
-        "code",
-        "table",
-        "horizontal-rule",
-        "undo",
-        "redo",
-      ],
-      initialValue,
-    });
+      // dynamic import to to support SSR
+      const EasyMDE = (await import("easymde")).default;
+      const easyMDE = new EasyMDE({
+        element: editor,
+        showIcons: [
+          "strikethrough",
+          "code",
+          "table",
+          "horizontal-rule",
+          "undo",
+          "redo",
+        ],
+        initialValue,
+        placeholder: "Start typing...",
+      });
 
-    setMde(easyMDE);
-    mdeRecordSetter(easyMDE);
-  }, []);
+      setMde(easyMDE);
+      mdeRecordSetter(easyMDE);
+    },
+    [initialValue, mdeRecordSetter]
+  );
 
   return (
     <div>
